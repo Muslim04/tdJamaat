7c42f1b56f85bd84cefb16569cbe1f1de6d75c43
Muslim04 <muslimabdyvapov@gmail.com>
2025-10-09 02:19:57 +0200

    s

--- src/TeamPerformanceTracker.tsx
+++ src/TeamPerformanceTracker.tsx
@@ -537,7 +537,7 @@ const TeamPerformanceTracker: React.FC = () => {
                                     <Tooltip />
                                     <Legend />
                                     <Bar dataKey="score" name="Упай">
-                                        {memberChartData.map((_entry, index) => {
+                                        {memberChartData.map((_, index) => {
                                             const member = currentTeamRankings[index];
                                             return <Cell key={`cell-${index}`} fill={COLORS[member.role]} />;
                                         })}
